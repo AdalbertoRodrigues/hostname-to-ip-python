6df49b90da475c6b88e7020c16ca03e9a858a085
Adalberto Teixeira <adalberto.teixeirajr@outlook.com>
2022-06-07 01:33:10 -0300

    Initial commit

---
+++ host_to_ip.py
@@ -0,0 +1,31 @@
+import  sys
+import socket
+import csv
+
+def hostToIp(hostname):
+    return socket.gethostbyname(hostname)
+
+def main():
+
+    try:
+        filePath = sys.argv[1]
+    except:
+        return
+        
+    ips = []
+
+    with open(filePath) as file:
+        csv_reader = csv.reader(file, delimiter=",")
+        for row in csv_reader:
+            ip = hostToIp(row[0])
+            ips.append("".join([row[0], ",", ip]))
+
+    print("\n".join(ips))
+
+main()
+
+
+
+
+
+
